4211374e9bb02e6866414368be11ca36f8ec5944
Diego Ordonez <diego_ord@hotmail.com>
2012-09-08 17:06:59 -0500

    Cambio en modelo usuario, se agrego el campo passwsord

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,4 @@
 class Usuario < ActiveRecord::Base
   
-  attr_accessible :apellido, :documento, :login, :nombres, :tipo_documento, :tipo_usuario
+  attr_accessible :apellido, :documento, :login, :nombres, :tipo_documento, :tipo_usuario, :password
 end
